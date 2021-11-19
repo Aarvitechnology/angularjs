d543f03149060074bd0415f8ceb6bbdd2db415ea
Parth Devani <parthdevani.aarvi@gmail.com>
2021-11-19 16:26:16 +0530

    all validation applied

--- insert.php
+++ insert.php
@@ -6,7 +6,7 @@ include('database_connection.php');
 
 $form_data = json_decode(file_get_contents("php://input"));
 
-$error = '';
+$error = array();
 $message = '';
 $validation_error = '';
 $first_name = '';
@@ -24,6 +24,11 @@ if($form_data->action == 'fetch_single_data')
 		$output['last_name'] = $row['last_name'];
 		$output['email'] = $row['email'];
 		$output['mobile'] = $row['mobile'];
+		$output['gender'] = $row['gender'];
+		$output['dob'] = $row['dob'];
+		$output['course'] = $row['course'];
+		$output['hobbies'] = $row['hobbies'];
+		$output['photo'] = $row['photo'];
 	}
 }
 else if($form_data->action == "Delete")
@@ -75,6 +80,62 @@ else
 		$mobile = $form_data->mobile;
 	}
 
+	if(empty($form_data->gender))
+	{
+		$error[] = 'gender is Required';
+	}
+	else
+	{
+		$gender = $form_data->gender;
+	}
+
+	if(empty($form_data->dob))
+	{
+		$error[] = 'dob is Required';
+	}
+	else
+	{
+		$dob = $form_data->dob;
+	}
+
+	if(empty($form_data->course))
+	{
+		$error[] = 'course is Required';
+	}
+	else
+	{
+		$course = $form_data->course;
+	}
+
+	if(empty($form_data->hobbies))
+	{
+		$error[] = 'hobbies is Required';
+	}
+	else
+	{
+		$hobbies = $form_data->hobbies;
+	}
+
+	if(empty($form_data->photo))
+	{
+		$error[] = 'photo is Required';
+	}
+	else
+	{
+		$filename = date("YmdHis"); 
+		$base64DataString = $form_data->photo;
+
+		list($dataType, $imageData) = explode(';', $base64DataString);
+		$imageExtension = explode('/', $dataType)[1];
+		$filename = $filename.'.'.$imageExtension;
+		list(, $encodedImageData) = explode(',', $imageData);
+		$decodedImageData = base64_decode($encodedImageData);
+		// save image data as file
+		file_put_contents("images/{$filename}", $decodedImageData);
+		$photo = "http://localhost/angularjs-1/images/{$filename}";
+	}
+
+
 	if(empty($error))
 	{
 		if($form_data->action == 'Insert')
@@ -83,12 +144,17 @@ else
 				':first_name'		=>	$first_name,
 				':last_name'		=>	$last_name,
 				':email'		=>	$email,
-				':mobile'		=>	$mobile
+				':mobile'		=>	$mobile,
+				':gender'		=>	$gender,
+				':dob'		=>	$dob,
+				':course'		=>	$course,
+				':hobbies'		=>	$hobbies,
+				':photo'		=>	$photo,
 			);
 			$query = "
 			INSERT INTO tbl_sample 
-				(first_name, last_name,email,mobile) VALUES 
-				(:first_name, :last_name, :email, :mobile)
+				(first_name, last_name,email,mobile,gender,dob,course,hobbies,photo) VALUES 
+				(:first_name, :last_name, :email, :mobile, :gender, :dob, :course, :hobbies, :photo)
 			";
 			$statement = $connect->prepare($query);
 			if($statement->execute($data))
@@ -103,11 +169,16 @@ else
 				':last_name'	=>	$last_name,
 				':email'	=>	$email,
 				':mobile'	=>	$mobile,
+				':gender'	=>	$gender,
+				':dob'	=>	$dob,
+				':course'	=>	$course,
+				':hobbies'	=>	$hobbies,
+				':photo'	=>	$photo,
 				':id'			=>	$form_data->id
 			);
 			$query = "
 			UPDATE tbl_sample 
-			SET first_name = :first_name, last_name = :last_name, email = :email, mobile = :mobile
+			SET first_name = :first_name, last_name = :last_name, email = :email, mobile = :mobile, gender = :gender, dob = :dob, course = :course, hobbies = :hobbies, photo = :photo
 			WHERE id = :id
 			";
 
@@ -129,6 +200,8 @@ else
 	);
 
 }
+
+
 
 
 
